f7337281bc371d5f7fe1d0b494db3dd9ef699ce3
CodingMan <CodingMan@uptt.cc>
2020-09-15 11:48:35 +0800

    Init commit

---
+++ src/errorcode.py
@@ -0,0 +1,12 @@
+
+
+class ErrorCode:
+    Success = 0
+    WrongIDPW = 1
+    LoginTooOften = 2
+    LoginFail = 3
+    BlackList = 4
+    NoSuchUser = 5
+    UserOffLine = 6
+    Unsupported = 7
+    TokenNotMatch = 8
