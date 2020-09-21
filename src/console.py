d686641b1e23a36f8d421a9c42793823b6952d6d
CodingMan <CodingMan@uptt.cc>
2020-09-21 16:32:26 +0800

    Move files to here

---
+++ src/console.py
@@ -0,0 +1,23 @@
+class Console:
+    def __init__(self):
+        # dev
+        # release
+
+        self.run_mode = 'dev'
+        self.config = None
+        self.event = None
+
+        ##################################
+        # client
+        self.command = None
+        self.dynamic_data = None
+
+        self.login_token = None
+        self.ptt_id = None
+        self.dialogue = None
+
+        ##################################
+
+        # server
+
+
