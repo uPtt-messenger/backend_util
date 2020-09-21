753617fc4a582550790931fc41ed4f41e841e669
CodingMan <CodingMan@uptt.cc>
2020-09-21 15:23:37 +0800

    ECC key change test ok

--- src/crypto.py
+++ src/crypto.py
@@ -2,19 +2,29 @@ from Crypto.PublicKey import ECC
 
 # https://pycryptodome.readthedocs.io/en/latest/src/public_key/ecc.html?highlight=EccPoint#Crypto.PublicKey.ECC.EccPoint
 
-key_type = [
-    'NIST P-521',
-    'p521',
-    'P-521',
-    'prime521v1',
-    'secp521r1'
+curve_type = [
+    'prime256v1'
 ]
 
-for key in key_type:
-    key = ECC.generate(curve=key)
+for curve in curve_type:
+    key_0 = ECC.generate(curve=curve)
+    key_1 = ECC.generate(curve=curve)
 
-    print(key.export_key(format='PEM'))
+    # print(key_0.export_key(format='PEM'))
+    # print(key_1.export_key(format='PEM'))
 
-    print(key.public_key().export_key(format='PEM'))
+    point_0 = key_0.public_key().pointQ
+    point_1 = key_1.public_key().pointQ
 
-    ECC.EccPoint
+    session_key_10 = point_1 * key_0.d
+    # print(session_key_10.xy)
+
+    session_key_01 = point_0 * key_1.d
+    # print(session_key_01.xy)
+
+    assert (session_key_10 == session_key_01)
+
+
+
+    print('= ' * 40)
+
